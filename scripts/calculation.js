1ab9e8de5d70aa6b8f65cc7f0f0671ba874157f4
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-10-05 19:37:51 +0500

    throw an error if age is less than lifespan

--- scripts/calculation.js
+++ scripts/calculation.js
@@ -4,8 +4,16 @@ const display = document.querySelector('#display');
 const calcBtn = document.querySelector('#calcBtn');
 
 function percentage(){
+  const ageVal = age.valueAsNumber;
+  const estAgeVal = estAge.valueAsNumber;
+  if (ageVal > estAgeVal){
+    console.log('age cannot be less than estAge');
+    display.innerHTML = `<p style='color: red;'>Age cannot be less than Life Span.</p>`;
+    return
+  }
+
   const calculate = ((age.valueAsNumber / estAge.valueAsNumber) * 100).toFixed(1);
   display.innerHTML = `<p>You've lived ${calculate}% Percent of your life.</p>`;
   age.value = estAge.value = '';
 }
-calcBtn.addEventListener('click', percentage)
+calcBtn.addEventListener('click', percentage);
