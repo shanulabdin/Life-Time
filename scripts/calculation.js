de6e41206062f3ff0b618ccd3ab3b391f66f1da1
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-10-06 19:17:35 +0500

    searching from the data

--- scripts/calculation.js
+++ scripts/calculation.js
@@ -21,3 +21,30 @@ function percentage(){
   age.value = estAge.value = '';
 }
 calcBtn.addEventListener('click', percentage);
+
+
+// Searching from Data
+import { data } from "./data.js";
+const countryInput = document.querySelector('#countryInput');
+const countryBtn = document.querySelector('#countryBtn');
+
+
+function submit(){
+  let country;
+  let countryInputVal = countryInput.value;
+
+  country = countryInputVal
+  
+  console.log(country, data[country]);
+}
+
+countryBtn.addEventListener('click', () => {
+    submit();
+    countryInput.value = '';
+});
+countryInput.addEventListener('keydown', (e) => {
+  if(e.key === 'Enter'){
+    submit();
+    countryInput.value = '';
+  }
+});
--- scripts/lifespans.js
+++ scripts/lifespans.js
@@ -1,11 +1,15 @@
 import { data } from "./data.js";
 
-let country = "Western Sahara";
-console.log(country, data[country]);
+// let country = "Western Sahara";
+// console.log(country, data[country]);
 
-let countries = Object.keys(data);
+// let countries = Object.keys(data);
 
-for(let i = 0; i < 10; i++){
-  console.log(`${i}`, countries[i]);
-}
+// for(let i = 0; i < 10; i++){
+//   console.log(`${i}`, countries[i]);
+// }
 
+
+
+// Data from:
+// https://www.worldometers.info/demographics/life-expectancy
