c17e2bcbd5007828b99693b051f4c24b76fc7835
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-10-07 10:17:57 +0500

    delete unused code

--- scripts/calculation.js
+++ scripts/calculation.js
@@ -35,31 +35,3 @@ function handleSubmit(e){
 
 countryBtn.addEventListener('click', handleSubmit);
 countryInput.addEventListener('keydown', handleSubmit);
-
-// Old code
-  // const maleAvg = data[country].male;
-  // const femaleAvg = data[country].female;
-
-  // life percentage
-  // const [age, estAge] = document.querySelectorAll('input');
-  // const display = document.querySelector('#display');
-  // const calcBtn = document.querySelector('#calcBtn');
-
-  // function percentage(){
-  //   const ageVal = age.valueAsNumber;
-  //   const estAgeVal = estAge.valueAsNumber;
-  //   if (ageVal > estAgeVal){
-  //     display.innerHTML = `<p style='color: red;'>Age cannot be less than Life Span.</p>`;
-  //     return;
-  //   }
-
-  //   if(ageVal <= 0){
-  //     display.innerHTML = `<p style='color: red;'>Age must be more than 0.</p>`;
-  //     return;
-  //   }
-
-  //   const calculate = ((ageVal / estAgeVal) * 100).toFixed(1);
-  //   display.innerHTML = `<p>You've lived ${calculate}% Percent of your life.</p>`;
-  //   age.value = estAge.value = '';
-  // }
-  // calcBtn.addEventListener('click', percentage);
