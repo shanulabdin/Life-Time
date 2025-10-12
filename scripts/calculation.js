2d16d45ecf4638b9f9ec9b96ef1e5fdcfd033dcd
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-10-12 12:53:35 +0500

    organising functions, gender functions done

--- scripts/calculation.js
+++ scripts/calculation.js
@@ -8,19 +8,36 @@ const countryInput = document.querySelector('#countryInput');
 const countryBtn = document.querySelector('#countryBtn');
 
 let country;
+let totalAvg;
+let calcPercentage;
+let gender;
+
+
+// Handle Submit Function
 function submit(){
   country = countryInput.value.trim().toLowerCase();
   countryInput.focus();
   percentage();
+  displayResult();
+  handleGenderInput();
 }
 
+// Calculate Percentage Function
 function percentage(){
   const ageInputVal = ageInput.value;
-  
-  let totalAvg;
-  
-  // Handle Gender Input
-  const genderInput = document.querySelector('#genderInput');
+
+  calcPercentage = ((ageInputVal / totalAvg) * 100).toFixed(2);
+  console.log(country.charAt(0).toUpperCase() + country.slice(1));
+}
+
+function displayResult(){
+  display.innerHTML = `<p>You have lived <strong>${calcPercentage}%</strong> of your estimated life as per average life expectancy of an average <strong>${genderInput.value}</strong> living in <strong>${country.charAt(0).toUpperCase() + country.slice(1)}</strong>, which is <strong>${totalAvg}</strong> years .</p>`;
+}
+
+// Handle Gender Input
+function handleGenderInput(){
+  const genderInputVal = document.querySelector('#genderInput');
+  genderInput = genderInputVal;
 
   if (genderInput.value === 'person'){
     totalAvg = data[country].total;
@@ -29,13 +46,7 @@ function percentage(){
   } else if (genderInput.value === 'female'){
     totalAvg = data[country].female;
   }
-
-  const calcPercentage = ((ageInputVal / totalAvg) * 100).toFixed(2);
-  console.log(country.charAt(0).toUpperCase() + country.slice(1));
-
-  display.innerHTML = `<p>You have lived <strong>${calcPercentage}%</strong> of your estimated life as per average life expectancy of an average <strong>${genderInput.value}</strong> living in <strong>${country.charAt(0).toUpperCase() + country.slice(1)}</strong>, which is <strong>${totalAvg}</strong> years .</p>`;
-} 
-
+}
 // Handle Submit Button
 function handleSubmit(e){
   if((e.type === 'keydown' && e.key === 'Enter') || e.type === 'click'){
