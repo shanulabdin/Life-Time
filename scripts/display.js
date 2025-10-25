63a8bb57a448de64ecb40ac66eeafe1df88d6967
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-10-25 23:40:41 +0500

    basic version ready

--- scripts/display.js
+++ scripts/display.js
@@ -13,27 +13,18 @@ try {
 const { age, gender, country, phone, sleep, work } = payload;
 console.log(payload)
 
-const out = document.getElementById('out');
-out.innerHTML =  `<p>Age: ${age}</p>
-                  <p>Gender: ${gender}</p>
-                  <p>Country: ${country}</p>
-                  <p>Phone Time: ${phone}</p>
-                  <p>Sleep: ${sleep}</p>
-                  <p>Work: ${work}</p>`;
-
 
 const lifeExpectancy = Math.round(data[country][gender]);
-console.log(lifeExpectancy);
 const sleepYears = Math.round((sleep / 24) * (lifeExpectancy - age));
-console.log(sleepYears);
 const workYears = Math.round((work / 24) * (lifeExpectancy - age));
-console.log(workYears);
+const phoneYears = Math.round((phone / 24) * (lifeExpectancy - age));
 
 function renderDots(lived, sleepYears){
   const container = document.getElementById('dots');
   
   let sleepAssigned = 0;
   let workAssigned = 0;
+  let phoneAssigned = 0;
 
   for(let i = 0; i < lifeExpectancy; i++){
     const d = document.createElement('span');
@@ -47,6 +38,9 @@ function renderDots(lived, sleepYears){
     } else if(workAssigned < workYears){
       d.classList.add('work');
       workAssigned++;
+    } else if(phoneAssigned < phoneYears){
+      d.classList.add('phone');
+      phoneAssigned++;
     }
     container.appendChild(d);
   }
