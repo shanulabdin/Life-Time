3491c3610caae552e97ac814bfb3989ad6204daf
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-10-25 23:31:04 +0500

    added the sleep dots

--- scripts/display.js
+++ scripts/display.js
@@ -1,3 +1,5 @@
+import { data } from '../scripts/data.js';
+
 const raw = localStorage.getItem('lifeTimeForm');
 
 let payload = {};
@@ -9,28 +11,38 @@ try {
 }
 
 const { age, gender, country, phone, sleep, work } = payload;
-console.log('payload: ', payload);
+console.log(payload)
 
-// const out = document.getElementById('out');
-// out.innerHTML =  `<p>Age: ${age}</p>
-//                   <p>Gender: ${gender}</p>
-//                   <p>Country: ${country}</p>
-//                   <p>Phone Time: ${phone}</p>
-//                   <p>Sleep: ${sleep}</p>
-//                   <p>Work: ${work}</p>`;
+const out = document.getElementById('out');
+out.innerHTML =  `<p>Age: ${age}</p>
+                  <p>Gender: ${gender}</p>
+                  <p>Country: ${country}</p>
+                  <p>Phone Time: ${phone}</p>
+                  <p>Sleep: ${sleep}</p>
+                  <p>Work: ${work}</p>`;
 
-function renderDots(filled){
-  const years = 100;
+
+const lifeExpectancy = Math.round(data[country][gender]);
+console.log(lifeExpectancy);
+const sleepYears = Math.round((sleep / 24) * (lifeExpectancy - age));
+console.log(sleepYears);
+
+function renderDots(lived, sleepYears){
   const container = document.getElementById('dots');
   
-  for(let i = 0; i < years; i++){
+  let sleepAssigned = 0;
+
+  for(let i = 0; i < lifeExpectancy; i++){
     const d = document.createElement('span');
     d.classList.add('dot');
-    container.appendChild(d);
-
-    if(i < filled){
+    
+    if(i < lived){
       d.classList.add('filled');
+    } else if(sleepAssigned < sleepYears){
+      d.classList.add('sleep');
+      sleepAssigned++;
     }
+    container.appendChild(d);
   }
 }
-renderDots(13);
+renderDots(age, sleepYears);
